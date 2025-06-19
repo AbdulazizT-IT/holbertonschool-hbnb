d91a0de7061e3126739eac62db3d98a18e2e9e88
Abdulaziz <94856585+AbdulazizT-IT@users.noreply.github.com>
2025-06-19 19:41:30 +0300

    Update review.py

--- part2/app/models/review.py
+++ part2/app/models/review.py
@@ -1,1 +1,17 @@
+from app.models.BaseModel import BaseModel
 
+class Review(BaseModel):
+    def __init__(self, text, rating, place, user):
+        super().__init__()
+        self.text = text
+        self.rating = rating
+        self.place = place
+        self.user = user
+
+    def add_review(self, review):
+        """Add a review to the place."""
+        self.reviews.append(review)
+
+    def add_amenity(self, amenity):
+        """Add an amenity to the place."""
+        self.amenities.append(amenity)
