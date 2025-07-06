b970d6124196ca25c6b04a13ecbf9c617ef2e162
Your Name <you@example.com>
2025-07-06 15:47:51 +0000

    edit for task 1

--- part3/app/api/v1/users.py
+++ part3/app/api/v1/users.py
@@ -1,5 +1,6 @@
 from flask_restx import Namespace, Resource, fields
 from app.services import facade
+from models.user import hash_password, verify_password
 
 
 api = Namespace('users', description='User operations')
@@ -8,7 +9,8 @@ api = Namespace('users', description='User operations')
 user_model = api.model('User', {
     'first_name': fields.String(required=True, description='First name of the user'),
     'last_name': fields.String(required=True, description='Last name of the user'),
-    'email': fields.String(required=True, description='Email of the user')
+    'email': fields.String(required=True, description='Email of the user'),
+    'password': fields.String(required=True, description='Password of the user')
 })
 
 @api.route('/')
@@ -39,6 +41,10 @@ class UserList(Resource):
         existing_user = facade.get_user_by_email(user_data['email'])
         if existing_user:
             return {'error': 'Email already registered'}, 400
+
+        # hash the password for user
+        hashed_password = hash_password(user_data["password"])
+        user_data["password"] = hashedpassword
 
         new_user = facade.create_user(user_data)
         return {'id': new_user.id, 'first_name': new_user.first_name, 'last_name': new_user.last_name, 'email': new_user.email}, 201
--- part3/app/models/user.py
+++ part3/app/models/user.py
@@ -9,6 +9,14 @@ class User(BaseModel):
     self.is_admin = is_admin
     self.places = []
 
-    def add_place(self, place):
-      """Add a place to the user."""
-      self.places.append(place)
+  def add_place(self, place):
+    """Add a place to the user."""
+    self.places.append(place)
+
+  def hash_password(self, password):
+    """Hashes the password before storing it."""
+    self.password = bcrypt.generate_password_hash(password).decode('utf-8')
+
+  def verify_password(self, password):
+    """Verifies if the provided password matches the hashed password."""
+    return bcrypt.check_password_hash(self.password, password)
