4e7562464824a27a45b32e8bd46814a97b039371
Your Name <you@example.com>
2025-07-10 09:10:10 +0000

    update user

--- part3/app/models/user.py
+++ part3/app/models/user.py
@@ -1,27 +1,28 @@
+from app import db
 from .base_model import BaseModel
 from flask_bcrypt import Bcrypt
 
 bcrypt = Bcrypt()
 
 class User(BaseModel):
-  def __init__(self, first_name, last_name, email, password, is_admin = False):
-    super().__init__()
-    self.first_name = first_name
-    self.last_name = last_name
-    self.email = email
-    self.password = password
-    self.is_admin = is_admin
-    self.places = []
+    __tablename__ = 'users'
 
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(120), nullable=False, unique=True)
+    password = db.Column(db.String(128), nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
 
-  def add_place(self, place):
-    """Add a place to the user."""
-    self.places.append(place)
+    # places = db.relationship('Place', backref='user', lazy=True)
 
-  def hash_password(password):
-    """Hashes the password before storing it."""
-    return bcrypt.generate_password_hash(password).decode('utf-8')
+    def add_place(self, place):
+        """Add a place to the user."""
+        self.places.append(place)
 
-  def verify_password(self, password):
-    """Verifies if the provided password matches the hashed password."""
-    return bcrypt.check_password_hash(self.password, password)
+    def hash_password(self, password):
+        """Hashes the password before storing it."""
+        self.password = bcrypt.generate_password_hash(password).decode('utf-8')
+
+    def verify_password(self, password):
+        """Verifies if the provided password matches the hashed password."""
+        return bcrypt.check_password_hash(self.password, password)
--- part3/app/persistence/repository.py
+++ part3/app/persistence/repository.py
@@ -1,4 +1,7 @@
 from abc import ABC, abstractmethod
+from app.models.user import User
+from app import db
+from app.persistence.repository import SQLAlchemyRepository
 
 class Repository(ABC):
     @abstractmethod
@@ -81,3 +84,10 @@ class SQLAlchemyRepository(Repository):
 
     def get_by_attribute(self, attr_name, attr_value):
         return self.model.query.filter(getattr(self.model, attr_name) == attr_value).first()
+
+class UserRepository(SQLAlchemyRepository):
+    def __init__(self):
+        super().__init__(User)
+
+    def get_user_by_email(self, email):
+        return self.model.query.filter_by(email=email).first()
