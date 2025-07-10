d969b09bdebe6d1a87c10211effc04d849333ce1
M0simi <mish5508@gmail.com>
2025-07-10 17:53:58 +0300

    Update user.py

--- part3/app/models/user.py
+++ part3/app/models/user.py
@@ -1,6 +1,6 @@
-from app import db
 from .base_model import BaseModel
 from flask_bcrypt import Bcrypt
+from app import db
 
 bcrypt = Bcrypt()
 
@@ -13,15 +13,9 @@ class User(BaseModel):
     password = db.Column(db.String(128), nullable=False)
     is_admin = db.Column(db.Boolean, default=False)
 
-    # places = db.relationship('Place', backref='user', lazy=True)
-
-    def add_place(self, place):
-        """Add a place to the user."""
-        self.places.append(place)
-
     def hash_password(self, password):
-        """Hashes the password before storing it."""
-        self.password = bcrypt.generate_password_hash(password).decode('utf-8')
+        """Hashes the password and returns it."""
+        return bcrypt.generate_password_hash(password).decode('utf-8')
 
     def verify_password(self, password):
         """Verifies if the provided password matches the hashed password."""
